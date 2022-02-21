a21e912abad4c5a106d632fefc8bb29685061789
Indrajit Sahu <indrajit70647@gmail.com>
2022-02-21 15:27:33 +0530

    joke1

---
+++ script.js
@@ -0,0 +1,11 @@
+// https://icanhazdadjoke.com/slack
+
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        let para = document.getElementById('joke');
+        para.innerHTML = jokeText;
+    })
+    
+
